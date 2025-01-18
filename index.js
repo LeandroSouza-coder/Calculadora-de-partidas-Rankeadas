932f90560e6811fc6d3cb16c2a255db5d40bab85
LeandroSouza-coder <83925147+LeandroSouza-coder@users.noreply.github.com>
2025-01-18 13:26:19 -0300

    Update index.js
    Desafio - Calculadora de partidas Rankeadas

---
+++ index.js
@@ -0,0 +1,27 @@
+function calcularRankeadas(vitorias, derrotas) {
+    // Calcula o saldo de vitórias
+    let saldoVitorias = vitorias - derrotas;
+
+    // Determina o nível do jogador com base no número de vitórias
+    let nivel;
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else if (vitorias >= 101) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Desconhecido";
+    }
+
+    // Exibe a mensagem final
+    console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`);
+}
